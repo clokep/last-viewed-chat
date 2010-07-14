71925453939d09ed9425b5fbd3bf68101d11da7a
clokep <clokep@gmail.com>
2010-07-14 19:09:07 -0400

    Update license

--- chrome/content/instantbird.js
+++ chrome/content/instantbird.js
@@ -20,7 +20,7 @@
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- *		Patrick Cloke (clokep-at-gmail-dot-com)
+ *		Patrick Cloke (clokep@gmailcom)
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
